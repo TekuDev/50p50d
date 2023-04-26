551e2fe4857c5cf6d17ff2028dff2019e6777a64
Teku Dev <teku.developer@gmail.com>
2023-04-26 20:39:20 +0200

    Day 4 - Hidden Search

---
+++ day4/script.js
@@ -0,0 +1,14 @@
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    /*if (search.classList.contains('active')) {
+        search.classList.remove('active')
+    } else {
+        search.classList.add('active')
+    } equivalent to: */
+    search.classList.toggle('active')
+    
+    input.focus()
+})
